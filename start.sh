56be830a226aa16fa511144f427b1f2a0ab19fc2
Marc Wäckerlin <marc@waeckerlin.org>
2016-11-06 17:47:14 +0100

    start for all remainig files at init

--- start.sh
+++ start.sh
@@ -4,6 +4,8 @@ for f in "${INPUT_DIR}" "${OUTPUT_DIR}" "${CONFIG_DIR}" "${TMP_DIR}"; do
     test -d "$f" || mkdir -p "$f"
 done
 
+/ocr.sh
+
 inotifywait -r -m --format '%w%f' -e close_write "${INPUT_DIR}" |
     while read filename; do
         /ocr.sh "${filename}"
